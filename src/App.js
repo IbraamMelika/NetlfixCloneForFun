b2e072e58f2e1739c8d6670191aa3e4f7efdeb60
ibraammelika <ibraam2009@gmail.com>
2021-03-29 16:28:37 -0400

    added more generes on screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css';
 //import Row  from './Row';
-
+//1:27:48
 import requests from "./requests"
 
 import RowRetry from './RowRetry'
@@ -12,6 +12,11 @@ function App() {
       <h1>Hey, Whats Up!</h1>
       <RowRetry title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetlfixOriginals}/>
       <RowRetry title="Trending Now" fetchURL={requests.fetchTrending}/>
+      <RowRetry title="Top Rated" fetchURL={requests.fetchTopRated}/>
+      <RowRetry title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+      <RowRetry title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+      <RowRetry title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+      <RowRetry title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
     </div>
   );
 }
